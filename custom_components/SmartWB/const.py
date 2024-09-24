ac8feb5fdaa8e804ea83a592b311c74f2340abb9
Dominik <89667597+DominikWrobel@users.noreply.github.com>
2024-09-24 13:44:11 +0200

    Create const.py
    v0.9

---
+++ custom_components/SmartWB/const.py
@@ -0,0 +1,10 @@
+"""Constants for the SmartWB integration."""
+DOMAIN = "smartwb"
+PLATFORMS = ["sensor", "switch", "number"]
+
+DEFAULT_NAME = "SmartWB"
+DEFAULT_SCAN_INTERVAL = 10
+DEFAULT_PORT = 80
+
+CONF_HOST = "host"
+CONF_PORT = "port"
